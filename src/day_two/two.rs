8c8afe52a5bd77d529e2727261fee46f814ff318
jcardoen <cardoen.joachim@gmail.com>
2021-12-02 08:50:19 +0100

    Day two

---
+++ src/day_two/two.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn get_lines(filename: &str) -> Vec<String> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    return reader.lines().map(|l| l.expect("Could not parse line")).collect();
+}
+
+pub fn star_one() -> i32 {
+
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    for line in get_lines("src/day_two/input.txt") {
+        let current_command_value: Vec<&str> = line.split_whitespace().collect();
+        let cmd = current_command_value[0];
+        let val = current_command_value[1].parse::<i32>().unwrap();
+        match cmd {
+            "forward" => horizontal += val,
+            "down" => depth += val,
+            "up" => depth -= val,
+            _ => println!("Nothing to match here for command {}", current_command_value[0])
+        };
+    }
+
+    return horizontal * depth;
+}
+
+pub fn star_two() -> i32 {
+
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+
+    for line in get_lines("src/day_two/input.txt") {
+        let current_command_value: Vec<&str> = line.split_whitespace().collect();
+        let cmd = current_command_value[0];
+        let val = current_command_value[1].parse::<i32>().unwrap();
+        match cmd {
+            "forward" => {horizontal += val; depth += (aim * val)},
+            "down" => aim += val,
+            "up" => aim -= val,
+            _ => println!("Nothing to match here for command {}", current_command_value[0])
+        };
+    }
+
+    return horizontal * depth;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 #[path = "day_one/one.rs"] mod day_one;
-
+#[path = "day_two/two.rs"] mod day_two;
 
 fn main() {
+    /* DAY ONE
     let counted = day_one::star_one();
     let counted_two = day_one::start_two();
 
     println!("{}", counted);
-    println!("{}", counted_two);
+    println!("{}", counted_two);*/
 
+
+    // DAY 2
+    //let position_one = day_two::star_one();
+    //println!("{}", position_one);
+
+    let position_two = day_two::star_two();
+
+    println!("{}", position_two);
 }
 
 
