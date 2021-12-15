fa56e2ac10dd7b7af196379547658804cd1cb607
jcardoen <cardoen.joachim@gmail.com>
2021-12-15 11:49:45 +0100

    star 1 ok

--- src/day_fifteen/fifteen.rs
+++ src/day_fifteen/fifteen.rs
@@ -1,5 +1,5 @@
 use std::cmp::min;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ptr::{hash, null};
@@ -14,7 +14,7 @@ fn get_lines(filename: &str) -> Vec<String> {
 }
 
 pub fn star_one() -> usize {
-    let input_lines = get_lines("src/day_fifteen/sample.txt");
+    let input_lines = get_lines("src/day_fifteen/input.txt");
     let mut grid: Vec<Vec<usize>> = vec![];
 
     for line in input_lines {
@@ -28,99 +28,52 @@ pub fn star_one() -> usize {
         grid.push(vector_for_line.clone());
     }
 
-    let mut path_cost: usize = 0;
-    let mut reached_end = false;
-    let mut current_i: usize = 0;
-    let mut current_j: usize = 0;
-    let mut visited: Vec<(usize, usize)> = vec![];
-    visited.push((0 as usize, 0 as usize));
-    while !reached_end {
-        path_cost += grid[current_i][current_j];
-        println!("Current position: {} {}", current_i, current_j);
-        let new_pos = set_next_position(&grid, current_i, current_j, &visited);
-        current_i = new_pos.0;
-        current_j = new_pos.1;        
-        visited.push((current_i, current_j));
+    let grid_size = grid.len();
 
-        if current_i == grid.len()-1 && current_j == grid.len()-1 {
-            reached_end = true;
-        }
-    }
-
-    return path_cost;
-}
-
-fn set_next_position(grid: &Vec<Vec<usize>>, mut current_i: usize, mut current_j: usize, visited: &Vec<(usize,usize)>) -> (usize, usize) {
-    let mut positions_to_check: Vec<(usize, usize)> = vec![];
-
-    if current_i == 0 {
-        if current_j == 0 {
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j+1));
-        }
-
-        else if current_j == grid.len()-1 {
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j-1));
-        }
-
-        else {
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j-1));
-            positions_to_check.push((current_i, current_j+1));
-        }
-    }
-    else if current_i == grid.len()-1 {
+    let mut costs = vec![vec![usize::MAX;grid_size];grid_size];
+    let mut neighbours: Vec<(usize, usize)> = vec![];
+    costs[0][0] = 0;
     
-        if current_j == 0 {
-            positions_to_check.push((current_i-1, current_j));
-            positions_to_check.push((current_i, current_j+1));
-        }
+    let mut queue = VecDeque::new();
 
-        else if current_j == grid.len()-1 {
-            positions_to_check.push((current_i, current_j-1));
-            positions_to_check.push((current_i-1, current_j));
-        } 
-        else {
-            positions_to_check.push((current_i-1, current_j));
-            positions_to_check.push((current_i, current_j-1));
-            positions_to_check.push((current_i, current_j+1));
-        }
-    }
-    else {
+    queue.push_front((0, 0));
 
-        if current_j == 0 {
-             
-            positions_to_check.push((current_i-1, current_j));
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j+1));
-        }
+    while queue.len() > 0 {
+        let current = queue.pop_front().unwrap();
+        println!("Determining cost of {} {}", current.0, current.1);
+        let neighbours = find_neighbours(current.0, current.1, grid_size);
 
-        else if current_j == grid.len()-1 {
-            positions_to_check.push((current_i-1, current_j));
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j-1));
-        } 
+        for neighbour in neighbours {
+            let cost_neighbour = costs[neighbour.0][neighbour.1];
+            let cost_to_reach_neighbour = costs[current.0][current.1] + grid[neighbour.0][neighbour.1];
 
-        else {
-        
-            positions_to_check.push((current_i-1, current_j));
-            positions_to_check.push((current_i+1, current_j));
-            positions_to_check.push((current_i, current_j-1));
-            positions_to_check.push((current_i, current_j+1));
-        }
-    }
-    let mut min: usize = usize::MAX;
-    for (i, j) in positions_to_check.iter() {
-        if !visited.contains(&(*i,*j)) {
-
-            if grid[*i][*j] <= min {
-                min = grid[*i][*j];
-                current_i = *i;
-                current_j = *j;
+            if cost_neighbour > cost_to_reach_neighbour {
+                costs[neighbour.0][neighbour.1] = cost_to_reach_neighbour;
+                queue.push_back(neighbour);
             }
         }
     }
-    println!("Next position should be {} {}", current_i, current_j);
-    return (current_i, current_j);
+
+    return costs[grid_size-1][grid_size-1];
+}
+
+
+fn find_neighbours(current_i: usize, current_j: usize, grid_size: usize) -> Vec<(usize, usize)> {
+    let mut positions_to_check: Vec<(usize, usize)> = vec![];
+    if current_i > 0 {
+        positions_to_check.push((current_i-1, current_j));
+    }
+    if current_i < grid_size-1 {
+        positions_to_check.push((current_i+1, current_j));
+    }
+
+    if current_j > 0 {
+        positions_to_check.push((current_i, current_j-1));
+    }
+
+    if current_j < grid_size-1 {
+        positions_to_check.push((current_i, current_j+1));
+    }
+
+    return positions_to_check;
 }
