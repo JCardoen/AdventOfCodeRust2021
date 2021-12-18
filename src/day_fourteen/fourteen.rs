106776fd752fd34c0e5b6c8088e68bf63cf0141d
jcardoen <cardoen.joachim@gmail.com>
2021-12-18 21:56:32 +0100

    cargo fmt

--- src/day_fourteen/fourteen.rs
+++ src/day_fourteen/fourteen.rs
@@ -47,7 +47,6 @@ pub fn star_one() -> usize {
         println!("{}", step_result);
     }
 
-
     let mut char_counter: HashMap<char, i32> = HashMap::new();
 
     for character in polymer_template.to_owned().chars() {
@@ -55,7 +54,7 @@ pub fn star_one() -> usize {
     }
 
     for (key, val) in char_counter {
-        println!{"{} {}", key, val};
+        println! {"{} {}", key, val};
     }
 
     return 0;
@@ -77,12 +76,18 @@ pub fn star_two() -> usize {
         }
     }
 
-    for (_, pair) in polymer_template.clone().chars().collect::<Vec<char>>().windows(2).enumerate() {
+    for (_, pair) in polymer_template
+        .clone()
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .enumerate()
+    {
         *pair_counter.entry((pair[0], pair[1])).or_insert(0) += 1;
     }
 
     for (key, val) in pair_counter.clone() {
-        println!{"{} {} {}", key.0, key.1, val};
+        println! {"{} {} {}", key.0, key.1, val};
     }
 
     //SHHBNFBCKNHCNOSHHVFF
@@ -90,25 +95,22 @@ pub fn star_two() -> usize {
     for pass in 0..40 {
         let mut intermediary_counter: HashMap<(char, char), usize> = HashMap::new();
 
-        for (p1, count) in pair_counter  {
+        for (p1, count) in pair_counter {
             let mut did_rule = false;
             for (rule, res) in &insertion_rules {
-                if *rule == format!(
-                    "{}{}",
-                    p1.0,
-                    p1.1
-                ) {
-                    *intermediary_counter.entry((p1.0, res.chars().nth(0).unwrap())).or_insert(0) += count;
-                    *intermediary_counter.entry((res.chars().nth(0).unwrap(), p1.1)).or_insert(0) += count;
+                if *rule == format!("{}{}", p1.0, p1.1) {
+                    *intermediary_counter
+                        .entry((p1.0, res.chars().nth(0).unwrap()))
+                        .or_insert(0) += count;
+                    *intermediary_counter
+                        .entry((res.chars().nth(0).unwrap(), p1.1))
+                        .or_insert(0) += count;
                     did_rule = true;
                 }
             }
-
-
         }
         pair_counter = intermediary_counter;
     }
-
 
     let mut char_counter: HashMap<char, usize> = HashMap::new();
 
@@ -119,12 +121,11 @@ pub fn star_two() -> usize {
     let mut count_vec: Vec<(&char, &usize)> = char_counter.iter().collect();
 
     for i in 0..count_vec.len() {
-        println!{"{} {}", count_vec[i].0, count_vec[i].1};
+        println! {"{} {}", count_vec[i].0, count_vec[i].1};
     }
 
-
     count_vec.sort_by(|a, b| b.1.cmp(a.1));
-    println!{"{} {}", count_vec[0].1, count_vec[count_vec.len()-1].1};
+    println! {"{} {}", count_vec[0].1, count_vec[count_vec.len()-1].1};
 
-    return count_vec[0].1 - count_vec[count_vec.len()-1].1 - 1; // minus 1 because last character is counted twice
+    return count_vec[0].1 - count_vec[count_vec.len() - 1].1 - 1; // minus 1 because last character is counted twice
 }
